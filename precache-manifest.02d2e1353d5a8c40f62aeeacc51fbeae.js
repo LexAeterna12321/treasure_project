fd991bd6de0cde5efb7b7ea25dc4df7c4d0ba08d
Lex Aeterna <lexaeterna12321@gmail.com>
2019-01-16 01:08:29 +0100

    Updates

---
+++ precache-manifest.02d2e1353d5a8c40f62aeeacc51fbeae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "370a48ff759e87222201",
+    "url": "/treasure_project/static/css/main.ad228065.chunk.css"
+  },
+  {
+    "revision": "370a48ff759e87222201",
+    "url": "/treasure_project/static/js/main.370a48ff.chunk.js"
+  },
+  {
+    "revision": "f22dbc0527b7bed928b1",
+    "url": "/treasure_project/static/js/1.f22dbc05.chunk.js"
+  },
+  {
+    "revision": "dc14acff11f75b212c33",
+    "url": "/treasure_project/static/js/runtime~main.dc14acff.js"
+  },
+  {
+    "revision": "0272273bc52f99526cea6c8b3968aa58",
+    "url": "/treasure_project/index.html"
+  }
+];
